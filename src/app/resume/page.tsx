ee8f96f9fdd83f9bb425b74746c568d3d8b1312a
Saikannan1805 <saisathish1805@gmail.com>
2026-08-19 13:26:24 -0400

    Update resume redirect link

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function ResumePage() {
-  redirect("https://drive.google.com/file/d/1mNCcH14i2FGp7bWwAio5R0C1fb8RfEMt/view?usp=sharing");
+  redirect("https://drive.google.com/file/d/1ccOUSj9MDIXkfAbgyNymM_qpbLss0DFt/view?usp=sharing");
 }
